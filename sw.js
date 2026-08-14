731c5bbef968a6ce155257d5e07ba9bc0fc67bcf
Professor 100 Destino <professor100destino@gmail.com>
2026-08-14 20:49:35 -0300

    Remover cache da versão pública antiga

--- sw.js
+++ sw.js
@@ -1,5 +1,3 @@
-const CACHE='serralheria-net-v2';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{if(event.request.mode==='navigate'){event.respondWith(Response.redirect('https://serralheria-net.professor100destino.workers.dev/',302));}});
